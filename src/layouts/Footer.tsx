9d7dc36f2da6d66a05a8ca4832606102308f18f3
melonmochi <melonmochi3@gmail.com>
2019-12-22 15:56:57 +0100

    🎄 Used Netlify for deploying

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,29 +3,49 @@ import { Icon } from "antd";
 import { DefaultFooter } from "@ant-design/pro-layout";
 
 const Footer: React.FC = () => (
-  <DefaultFooter
-    copyright="2019 melonmochi"
-    links={[
-      {
-        key: "React TypeScript Boilerplate",
-        title: "React TypeScript Boilerplate",
-        href: "https://github.com/melonmochi/react-typescript-boilerplate",
-        blankTarget: true
-      },
-      {
-        key: "github",
-        title: <Icon type="github" />,
-        href: "https://github.com/melonmochi/react-typescript-boilerplate",
-        blankTarget: true
-      },
-      {
-        key: "Paradigma Digital",
-        title: "Paradigma Digital",
-        href: "https://www.paradigmadigital.com/",
-        blankTarget: true
-      }
-    ]}
-  />
+  <>
+    <DefaultFooter
+      copyright="2019 melonmochi"
+      links={[
+        {
+          key: "React TypeScript Boilerplate",
+          title: "React TypeScript Boilerplate",
+          href: "https://github.com/melonmochi/react-typescript-boilerplate",
+          blankTarget: true
+        },
+        {
+          key: "github",
+          title: <Icon type="github" />,
+          href: "https://github.com/melonmochi/react-typescript-boilerplate",
+          blankTarget: true
+        },
+        {
+          key: "Paradigma Digital",
+          title: "Paradigma Digital",
+          href: "https://www.paradigmadigital.com/",
+          blankTarget: true
+        }
+      ]}
+    />
+    <div
+      style={{
+        padding: "0px 24px 24px",
+        textAlign: "center"
+      }}
+    >
+      <a
+        href="https://www.netlify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
+          width="82px"
+          alt="netlify logo"
+        />
+      </a>
+    </div>
+  </>
 );
 
 export default Footer;
